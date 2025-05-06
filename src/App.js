64999fb3a4dcf589d2f4ffaae6e73aaae5262b70
m13t1c <milenko.letic@toptal.com>
2025-05-06 21:43:15 +0200

    add react hello-world-app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>Hello World from React via Amplify!</h1>
+    </div>
+  );
+}
+
+export default App;
+
